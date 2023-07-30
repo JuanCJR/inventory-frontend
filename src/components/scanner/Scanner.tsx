16d39837915e6675872db4a716580028f2c9c227
Juan C Jimenez <jcjimenezr@falabella.cl>
2023-07-30 13:41:05 -0400

    add device select

--- src/components/scanner/Scanner.tsx
+++ src/components/scanner/Scanner.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useMediaDevices } from "react-media-devices";
 import { useZxing } from "react-zxing";
 import styles from "./Scanner.module.css";
+import { FormControl, FormLabel, Select } from "@chakra-ui/react";
 const constraints: MediaStreamConstraints = {
   video: true,
   audio: false,
@@ -16,10 +17,13 @@ export const Scanner = (props: ScannerProps) => {
   const { handleChangeCode } = props;
   const [result, setResult] = useState("");
   const { devices } = useMediaDevices({ constraints });
+
   const deviceId = devices?.[2]?.deviceId;
+  const [devicesUsed, setDevicesUsed] = useState(deviceId);
+
   const { ref } = useZxing({
     // paused: !deviceId,
-    // deviceId,
+    deviceId: devicesUsed,
     onResult(result) {
       handleChangeCode(result.getText());
     },
@@ -31,6 +35,22 @@ export const Scanner = (props: ScannerProps) => {
         <div className={styles["line-horizontal"]}></div>
         <div className={styles["line-vertical"]}></div>
       </div>
+      <FormControl mt={2}>
+        <FormLabel>Camara</FormLabel>
+        <Select
+          placeholder="Seleccionar dispositivo"
+          onChange={(e) => {
+            const newdeviceId = e.target.value;
+            setDevicesUsed(newdeviceId);
+          }}
+        >
+          {devices?.map((device) => (
+            <option key={device.deviceId} value={device.deviceId}>
+              {device.label || device.kind}
+            </option>
+          ))}
+        </Select>
+      </FormControl>
     </div>
   );
 };
